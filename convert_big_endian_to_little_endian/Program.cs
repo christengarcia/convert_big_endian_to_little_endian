4bb0859d66b00a850e5fd24cb2d3c8a1c1b18b23
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-29 20:48:35 +1100

    First commit

---
+++ convert_big_endian_to_little_endian/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ *  C# Program to Convert Big Endian to Little Endian
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convert_big_endian_to_little_endian
+{
+    class Program
+    {
+        static int ReverseBytes(int val)
+        {
+            byte[] intAsBytes = BitConverter.GetBytes(val);
+            Array.Reverse(intAsBytes);
+            return BitConverter.ToInt32(intAsBytes, 0);
+        }
+        static string IntToBinaryString(int v)
+        {
+            string s = Convert.ToString(v, 2);
+            string t = s.PadLeft(32, '0');
+            string res = "";
+            for (int i = 0; i < t.Length; ++i)
+            {
+                if (i > 0 && i % 8 == 0)
+                    res += " ";
+                res += t[i];
+            }
+            return res;
+        }
+
+
+    }
+}
